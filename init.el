efdb8fe8132ef16af7808d74d79cd14667208a4d
bjuin <bjuin@debian>
2015-12-12 10:01:53 +0100

    linux conf

--- init.el
+++ init.el
@@ -1,24 +1,21 @@
-(setq make-backup-files nil) 
-(add-to-list 'load-path "~/.emacs.d/auto-comp-install/")
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-comp-install//ac-dict")
-(ac-config-default)
+(setq make-backup-files nil)
+(setq create-lockfiles nil)
+
+;(add-to-list 'load-path "~/.emacs.d/auto-comp-install/")
+;(require 'auto-complete-config)
+;(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-comp-install//ac-dict")
+;(ac-config-default)
 
 (windmove-default-keybindings)
 (ido-mode)
 
+;(define-key input-decode-map "\e[D" [S-left])
+;(define-key input-decode-map "\e[C" [S-right])
+;(define-key input-decode-map "\e[B" [S-down])
+;(define-key input-decode-map "\e[A" [S-up])
+;(define-key input-decode-map "\e[F" [S-end])
+;(define-key input-decode-map "\e[H" [S-home])
 
-
-
-
-(define-key input-decode-map "\e[D" [S-left])
-(define-key input-decode-map "\e[C" [S-right])
-(define-key input-decode-map "\e[B" [S-down])
-(define-key input-decode-map "\e[A" [S-up])
-(define-key input-decode-map "\e[F" [S-end])
-(define-key input-decode-map "\e[H" [S-home])
-
-(setq create-lockfiles nil)
 
 ;; MELPA REPO
 (require 'package) ;; You might already have this line
@@ -28,3 +25,7 @@
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize) ;; You might already have this line
+
+
+(custom-set-variables '(coffee-tab-width 4))
+(custom-set-variables '(jade-tab-width 4))
